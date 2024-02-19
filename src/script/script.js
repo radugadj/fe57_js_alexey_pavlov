720161cba23acd7919792f0ff3809f5a559ac489
radugadj <radugadj@gmail.com>
2024-02-19 21:27:05 +0300

    feat: add home work

--- src/script/script.js
+++ src/script/script.js
@@ -3,4 +3,66 @@
 // alert("hello world");
 
 
+// Задание 1
+let x = 20;
+let y = 58;
+let z = 42;
+
+console.log(x + y + z);
+
+// Задание 2
+
+const secondsInMinute = 60;
+const minutesInHour = 60;
+const hoursInDay = 24;
+const daysInYear = 365;
+
+const myAgeInYears = 32; 
+
+const myAgeInSeconds = myAgeInYears * daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+
+console.log("Мой возраст в секундах:", myAgeInSeconds);
+
+
+// Задание 3
+
+let count = 42;
+let userName = '42';
+
+let countAsString = count.toString();
+let userNameAsNumber = parseInt(userName);
+
+console.log("count как строка:", countAsString); 
+console.log("userName как число:", userNameAsNumber); 
+
+// Задание 3 вариант 2
+
+let count2 = 42;
+let userName2 = '42';
+
+let countAsString2 = '' + count;
+let userNameAsNumber2 = + userName;
+
+console.log("count как строка:", countAsString2);
+console.log("userName как число:", userNameAsNumber2); 
+
+
+// Задание 4
+let a = 1;
+let b = 2;
+let c = ' белых медведей';
+
+let result = a.toString() + b.toString() + c;
+
+console.log(result); 
+
+// Задание 5
+
+
+
+// Задание 6
+
+
+
+// Задание 7
 
